bb1153ab65fccacbb5687c99dc2c5e752ffb117d
Diana Laptop <diany1186@hotmail.com>
2023-05-04 21:36:20 -0500

    usando fetch

--- service/client-service.js
+++ service/client-service.js
@@ -1,35 +1,3 @@
-const crearNuevaLinea = (nombre,email)=> {
-    const linea = document.createElement("tr");
-    const contenido = `
-    <td class="td" data-td>${nombre}</td>
-    <td>${email}</td>
-    <td>
-        <ul class="table__button-control">
-            <li>
-                <a
-                    href="../screens/editar_cliente.html"
-                    class="simple-button simple-button--edit"
-                    >Editar</a
-                >
-            </li>
-            <li>
-                <button
-                class="simple-button simple-button--delete"
-                type="button"
-                >
-                Eliminar
-                </button>
-            </li>
-        </ul>
-    </td>`;
-    linea.innerHTML = contenido;
-    return linea;
-};
-
-const table = document.querySelector("[data-table]");
-
-
-
 /*Abrir http(método,url) 
 
 CRUD        Método HTTP
@@ -39,30 +7,9 @@ Update      -PUT/PATCH
 Delete      -DELETE
 */
 
-const listaClientes = () => { 
-    const promise = new Promise ((resolve,reject) => {
-        const http =new XMLHttpRequest();
-        http.open("GET","http://localhost:3000/perfil");
+//FETCH API (Sirve para manejar las promesas de forma abreviada)
+const listaClientes = () => fetch("http://localhost:3000/perfil").then( respuesta => respuesta.json());
 
-        http.send();
-
-        http.onload = () => {
-            const response = JSON.parse(http.response);
-            if(http.status >= 400) {
-                reject(response);
-            }else {
-                resolve(response);
-            }           
-        };
-    });
-    return promise;
+export const clientServices = {
+    listaClientes,      //En las nuevas versiones de js solo se define el objeto una vez y sirve como llave y como valor
 }
-
-listaClientes().then((data)=>{
-    console.log(data);
-    data.forEach((perfil) => {
-        const nuevaLinea = crearNuevaLinea(perfil.nombre,perfil.email);
-        table.appendChild(nuevaLinea);
-    });
-})
-.catch((error)=>alert("Ocurrió un error"));
