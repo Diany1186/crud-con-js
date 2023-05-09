86351bda18b81e1b68a8e91667a54bd76a6b10c3
Diana Laptop <diany1186@hotmail.com>
2023-05-08 23:48:10 -0500

    implementandó la insersión de un cliente

--- service/client-service.js
+++ service/client-service.js
@@ -1,15 +1,17 @@
-/*Abrir http(método,url) 
 
-CRUD        Método HTTP
-Create      -POST
-Read        -GET
-Update      -PUT/PATCH
-Delete      -DELETE
-*/
-
-//FETCH API (Sirve para manejar las promesas de forma abreviada)
 const listaClientes = () => fetch("http://localhost:3000/perfil").then( respuesta => respuesta.json());
+
+const crearCliente = (nombre,email) => {
+    return fetch("http://localhost:3000/perfil", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({nombre, email, id: uuid.v4()})
+    });
+};
 
 export const clientServices = {
     listaClientes,      //En las nuevas versiones de js solo se define el objeto una vez y sirve como llave y como valor
-}
+    crearCliente,
+};
